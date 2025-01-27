baeaadcc3d948fc2cf66eec9eeb4ffa7549d30a0
“emilesherrott” <emilesherrott@gmail.com>
2025-01-27 15:50:30 +0000

    Add completed microservices-code

--- week-4/taster-cloud-devops/docker-as-a-microservice/completed-code/countries-api/server/db/connect.js
+++ week-4/taster-cloud-devops/docker-as-a-microservice/completed-code/countries-api/server/db/connect.js
@@ -1,7 +1,11 @@
-const { Pool } = require("pg")
+const { Pool } = require('pg');
 
 const db = new Pool({
-    connectionString: process.env.DB_URL,
-})
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT || 5432,
+});
 
-module.exports = db
+module.exports = db;
